530919e983c01ee36403074a81bcf54708497c44
web3ten0 <web3ten0@gmail.com>
2023-03-01 12:56:24 +0900

    changed loggerMiddleware to function

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HogeModule } from './hoge/hoge.module';
-import { LoggerMiddleware } from './middleware/logger.middleware';
+import { logger } from './middleware/logger.middleware';
 
 @Module({
   imports: [HogeModule],
@@ -11,6 +11,6 @@ import { LoggerMiddleware } from './middleware/logger.middleware';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('hoge');
+    consumer.apply(logger).forRoutes('hoge');
   }
 }
