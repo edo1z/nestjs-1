4e17a787f298212fff5415e2a696f5487a782d6f
web3ten0 <web3ten0@gmail.com>
2023-03-01 12:35:42 +0900

    added middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { HogeModule } from './hoge/hoge.module';
 import { AppService } from './app.service';
+import { HogeModule } from './hoge/hoge.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [HogeModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('hoge');
+  }
+}
