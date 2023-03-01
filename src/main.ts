94972a5916146b758d6aa09d7be7f6a42a82e77f
web3ten0 <web3ten0@gmail.com>
2023-03-01 21:25:06 +0900

    added swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { INestApplication } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  swaggerSetup(app);
   await app.listen(3000);
 }
+
+function swaggerSetup(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Hoge')
+    .setDescription('The Hoge API description')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
+
 bootstrap();
