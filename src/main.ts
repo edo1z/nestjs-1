2a6873e852cb445fe495a6881b29fbfab82f5623
web3ten0 <web3ten0@gmail.com>
2023-03-03 14:07:37 +0900

    added prisma client exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   swaggerSetup(app);
+  setPrismaClientExceptionFilter(app);
   await app.listen(3000);
 }
 
@@ -19,6 +21,11 @@ function swaggerSetup(app: INestApplication) {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+}
+
+function setPrismaClientExceptionFilter(app: INestApplication) {
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 }
 
 bootstrap();
