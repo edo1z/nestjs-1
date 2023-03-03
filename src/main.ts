187c8ed650d8f3af3a53adfb5195e0cb6a1345a1
web3ten0 <web3ten0@gmail.com>
2023-03-03 10:39:24 +0900

    added validation of creating article

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   swaggerSetup(app);
   await app.listen(3000);
 }
